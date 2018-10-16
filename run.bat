a0438d14bf4bf296af7a88013c14497d479e63dd
a.churilin <ch257@yandex.ru>
2018-10-16 10:59:58 +0500

    first

---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+chcp 65001
+
+php scripts/main.php
+
+Pause
---
+++ scripts/main.php
@@ -0,0 +1,7 @@
+<?php
+require('config/config.php'); 
+require('scripts/modules/Host.php'); 
+require('scripts/modules/DB.php'); 
+require('scripts/modules/Tools.php'); 
+require('scripts/modules/Errors.php'); 
+print(TEST_CONST . "\n");
