158097496fc48a4f6558fa1805bab0976d54df3a
home <home@home.home>
2018-10-31 23:07:32 +0500

    tree test

--- scripts/test.php
+++ scripts/test.php
@@ -13,14 +13,6 @@ class Test {
 
 	}
 
-	public function ToolsTest($arr) {
-		$output = [];
-		$this->tools->TreeToPlain($arr, $output);
-		print_r($output);
-
-		print("\nOK\n");
-	}
-
 	public function DBTest($params) {
 		$this->db->connect_db($params);
 	}
@@ -54,15 +46,19 @@ class Test {
 		}
 	}
 	
-	public function TreeToFlat(&$tree, $parent_id=null) {
+	public function TreeToFlat(&$tree, $childe_key, $parent_index, $index, $parent_id=null) {
 		foreach ($tree as &$item) {
-			$flat[] = [
-				'id' => $item['id'],
-				'parent_id' => $parent_id,
-				'num' => $item['num']
-			];
-			if (isset($item['tree'])) {
-				$leaves = $this->TreeToFlat($item['tree'], $item['id']);
+			$leaf = [];
+			foreach ($item as $key=>$value) {
+				if ($key != $childe_key) {	
+					$leaf[$key] = $value;
+				}
+			}
+			$leaf[$parent_index] = $parent_id;
+			$flat[] = $leaf;
+			
+			if (isset($item[$childe_key])) {
+				$leaves = $this->TreeToFlat($item[$childe_key], $childe_key, $parent_index, $index, $item[$index]);
 				foreach ($leaves as $leaf) {
 					$flat[] = $leaf;			
 				}
@@ -71,15 +67,22 @@ class Test {
 		return $flat;
 	}
 	
-	public function FlatToTree(&$flat, $id=null) {
+	public function FlatToTree(&$flat, $childe_key, $parent_index, $index, $id=null) {
 		$tree = null;
 		foreach ($flat as &$item) {
-			if (!(isset($id) || isset($item['parent_id'])) || $item['parent_id'] == $id) { 
-				$tree[] = [
-					'id' => $item['id'],
-					'num' => $item['num'],
-					'tree' => $this->FlatToTree($flat, $item['id'])
-				];
+			if (!(isset($id) || isset($item[$parent_index])) || $item[$parent_index] == $id) { 
+				$branch = [];
+				foreach ($item as $key=>$value) {
+					if ($key != $parent_index) {	
+						$branch[$key] = $value;
+					}
+				}
+				
+				$childe = $this->FlatToTree($flat, $childe_key, $parent_index, $index, $item[$index]);
+				if (isset($childe)) {	
+					$branch[$childe_key] = $childe;
+				}
+				$tree[] = $branch;
 			}
 		}
 		return $tree;
@@ -100,15 +103,6 @@ $params = [
 ];
 //$test->DBTest($params);
 
-// --------- Tools test
-$arr = [
-	'one' => [
-		'one_one' => '1',
-		'one_two' => '2',
-	],
-	'three' => '3'
-];
-// $test->ToolsTest($arr);
 
 $tree = [
 	[
@@ -159,16 +153,16 @@ $tree = [
 	]
 ];
 
-$flat = $test->TreeToFlat($tree);
+$flat = $test->TreeToFlat($tree, 'tree', 'parent_id', 'id');
 // print_r($flat);
-$tree = $test->FlatToTree($flat);
-// print_r($tree);
-
-$flat = [];
-$test->TreeToFlatRef($tree, $flat);
-// print_r($flat);
-$tree = [];
-$test->FlatToTreeRef($flat, $tree);
+$tree = $test->FlatToTree($flat, 'events', 'parent_id', 'id');
 print_r($tree);
+
+// $flat = [];
+// $test->TreeToFlatRef($tree, $flat);
+// print_r($flat);
+// $tree = [];
+// $test->FlatToTreeRef($flat, $tree);
+// print_r($tree);
 
 
